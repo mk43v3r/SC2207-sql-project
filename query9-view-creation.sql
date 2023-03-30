9369624f97afd737671d2d218044bb3cf08721f3
Adrian Alviento <AlvientoAdrian.AA@gmail.com>
2023-03-30 21:28:54 +0800

    Delete query9-view-creation.sql

--- query9-view-creation.sql
+++
@@ -1,5 +0,0 @@
--- creates a view of monthly sales of publications 
-CREATE VIEW MonthlySales AS
-SELECT s.PubID, MONTH(o.Order_Date_time) as month, YEAR(o.Order_Date_time) as year, SUM(item_qty) as numSales
-FROM ( (Items_In_Order as i JOIN Stocks_In_Bookstore AS s ON i.StockID = s.StockID) JOIN Publication as p ON s.PubID = p.PubID) JOIN  Orders as o ON i.OrderID = o.OrderID 
-GROUP BY s.PubID, MONTH(o.Order_Date_time), YEAR(o.Order_Date_time);
