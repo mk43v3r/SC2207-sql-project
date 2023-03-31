0d22b5690e8b5847d7728f93234fdf5797f66bad
mk43v3r <mk43v3r@gmail.com>
2023-03-31 17:07:48 +0800

    Updated insert-test-data.sql

--- insert-test-data.sql
+++ insert-test-data.sql
@@ -1,59 +1,58 @@
 DELETE FROM Publication;
+DELETE FROM Magazines;
+DELETE FROM Books;
 DELETE FROM Employees;
 DELETE FROM Bookstore;
 DELETE FROM Customers;
 
 -- Publication test data insertion
 INSERT INTO Publication
-VALUES(1, 'p_a', 2001);
+VALUES
+	(1, 'p_a', 2001),
+	(2, 'p_b', 2002),
+	(3, 'p_c', 2003),
+	(4, 'Nanyang Publisher Company', 2004),
+	(5, 'Nanyang Publisher Company', 2004),
+	(6, 'Nanyang Publisher Company', 2004);
 
-INSERT INTO Publication
-VALUES(2, 'p_b', 2002);
 
-INSERT INTO Publication
-VALUES(3, 'p_c', 2003);
+-- Magazines test data insertion
+INSERT INTO Magazines
+VALUES
+	(2, 'm_a', 1),
+	(3, 'm_b', 2),
+	(6, 'm_c', 3);
 
-INSERT INTO Publication
-VALUES(4, 'Nanyang Publisher Company', 2004);
 
+-- Books test data insertion
+INSERT INTO Books
+VALUES
+	(1, 'Harry Porter Finale'),
+	(4, 'b_a'),
+	(5, 'b_b');
 
 -- Employees test data insertion
 INSERT INTO Employees
-VALUES(1, 'e_a' , 1000);
-
-INSERT INTO Employees
-VALUES(2, 'e_b' , 2000);
-
-INSERT INTO Employees
-VALUES(3, 'e_c' , 1000);
-
-INSERT INTO Employees
-VALUES(4, 'e_d' , 4000);
+VALUES
+	(1, 'e_a' , 1000),
+	(2, 'e_b' , 2000),
+	(3, 'e_c' , 1000),
+	(4, 'e_d' , 4000);
 
 
 -- Bookstore test data insertion
 INSERT INTO Bookstore
-VALUES(1);
-
-INSERT INTO Bookstore
-VALUES(2);
-
-INSERT INTO Bookstore
-VALUES(3);
-
-INSERT INTO Bookstore
-VALUES(4);
+VALUES
+	(1),
+	(2),
+	(3),
+	(4);
 
 
 -- Customers test data insertion
 INSERT INTO Customers
-VALUES(1, 'c_a')
-
-INSERT INTO Customers
-VALUES(2, 'c_b')
-
-INSERT INTO Customers
-VALUES(3, 'c_c')
-
-INSERT INTO Customers
-VALUES(4, 'c_d')
+VALUES
+	(1, 'c_a'),
+	(2, 'c_b'),
+	(3, 'c_c'),
+	(4, 'c_d');
