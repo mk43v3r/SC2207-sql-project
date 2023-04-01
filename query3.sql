83faf3d07f699c3434393138ba6de56c87a90277
Adrian Alviento <AlvientoAdrian.AA@gmail.com>
2023-04-01 15:29:29 +0800

    Update query3.sql
    query only requires the average date over all sold publications, thus, now excludes the grouping by pubID. Also simplified the query further. Output works as expected from the test data inserted

--- query3.sql
+++ query3.sql
@@ -1,11 +1,9 @@
 -- 3. For all publications purchased in June 2022 that have been
 -- delivered, find the average time from the ordering date to the
 -- delivery date.
-SELECT P.PubID, AVG(DATEDIFF(day, CAST(O.Order_Date_time AS DATE), I.Delivery_date)) AS Avg_Delivery_Time
-FROM Orders AS O
-JOIN Items_In_Order AS I ON O.OrderID = I.OrderID
-JOIN Stocks_In_Bookstore AS S ON I.StockID = S.StockID
-JOIN Publication as P ON S.PubID = P.PubID
-WHERE MONTH(O.Order_Date_time) = 6 AND YEAR(O.Order_Date_time) = 2022 AND
-		I.Delivery_date IS NOT NULL
-GROUP BY P.PubID
+
+SELECT AVG(DATEDIFF(day, O.Order_Date_time, IIO.Delivery_date)) as Average_Delivery_Time
+FROM Orders O
+JOIN Items_In_Order IIO ON O.OrderID = IIO.OrderID
+WHERE IIO.Delivery_date IS NOT NULL
+AND O.Order_Date_time >= '2022-06-01' AND O.Order_Date_time < '2022-07-01';
